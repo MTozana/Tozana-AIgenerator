8e65e7ef95e557e2e0d866509837337ec362d5d6
MTozana <violettozana@gmail.com>
2025-01-25 12:15:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function generatedestination(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
     let context = "Your are a travelling agent. Your mission is to generate holiday destinations all around the world including affordable accommodation and activities that can be done at the destination location. The destination must be provided in HTML format.";
-    let prompt = instructionsInput.value || "Generate holiday destinations and activities.";
+    let prompt = instructionsInput.value || "Generate a holiday destination";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
     try {
